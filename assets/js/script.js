c1e2b0d69d66882147cbf31e2f6b8e4b56bb4c8e
GooldenApple <apple.isberg@live.se>
2025-10-08 11:36:11 +0200

    add code for division gamw

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if(gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     }
      else {
         alert(`Unknown game type: ${gameType}`);
@@ -149,9 +151,10 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 function displayDivisionQuestion(operand1, operand2) {
 
+    operand1 = operand1 * operand2;
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "/";
-
+    
 
 }
